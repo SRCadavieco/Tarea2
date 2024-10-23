03d5b38c68ce26d6db6c98fad0bb12fa69cc0aea
Sergio <sergiorc2694@gmail.com>
2024-10-23 10:54:57 +0200

    Creacion metodo crearProfesoresDeMuestra

--- clases/Profesores.php
+++ clases/Profesores.php
@@ -8,6 +8,22 @@ class Profesores extends Miembro{
         $this->titular=true;
         $this->asignatura=$asignatura;
     }
+    function crearProfesoresDeMuestra(){
+        $profesores = array(); // Inicializas el array vacío
+
+        $p1 = new Profesores(1, "Steve", "Wozniak", "steve@apple.com", "DWES");
+        $profesores[] = $p1;
+        
+        $p2 = new Profesores(2, "Ada", "Lovelace", "ada@gmail.com", "DIW");
+        $profesores[] = $p2;
+        
+        $p3 = new Profesores(3, "Taylor", "Otwell", "taylor@laravel.com", "DWEC");
+        $profesores[] = $p3;
+        
+        $p4 = new Profesores(4, "Rasmus", "Lerdoff", "rasmus@php.com", "DAW");
+        $profesores[] = $p4;
+        
+        return $profesores;}
 
 
 
