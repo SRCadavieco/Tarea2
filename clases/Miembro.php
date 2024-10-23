7d390a33fd5aad919d6a29b5c8d6799b71947dbb
Sergio <sergiorc2694@gmail.com>
2024-10-23 09:36:52 +0200

    Creacion clase Miembro + Getters y setters + Constructor

---
+++ clases/Miembro.php
@@ -0,0 +1,118 @@
+<?php
+    abstract Class Miembro{
+    protected $id;
+    protected $nombre;
+    protected $apellidos;
+    protected $email;
+    protected $edad;
+
+    public function __construct($id,$nombre,$apellidos,$email,$edad){
+        $this->$id = $id;
+        $this->$nombre=$nombre;
+        $this->$apellidos=$apellidos;
+        $this->$email=$email;
+        $this->edad=$edad;
+    }
+
+
+
+
+
+
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     */
+    public function setNombre($nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellidos
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set the value of apellidos
+     */
+    public function setApellidos($apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of edad
+     */
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * Set the value of edad
+     */
+    public function setEdad($edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+    public function __toString()
+{
+    return  "Nombre: ".$this->nombre . " " . $this->apellidos  . "Email: " .  $this->email;
+}
+    }
+    
